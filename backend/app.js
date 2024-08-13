e6dadc33502a85e7daa73c04d8110a7a2106ae0e
srimaran712 <73399705+srimaran712@users.noreply.github.com>
2024-08-13 19:40:04 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const Mongoose=require('mongoose')
 app.use(bodyParser.json())
 //https://create-react-app.dev/docs/deployment/
 app.use(Cors({
-  origin: 'https://classroom-frontendredeploy.onrender.com', 
+  origin: ['https://classroom-frontendredeploy.onrender.com','http://localhost:3000'], 
 }))
 
 //connecting to database
